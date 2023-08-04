9c091a55ada6882986e1441a987657917614d098
Raul <raulmarian1106@gmail.com>
2023-08-04 17:45:40 +0300

    Deleting useless things

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import About from "./About";
+import WorkExperience from "./WorkExperience";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+
+const components = [Header, Hero, About, WorkExperience, Skills, Projects, ContactMe];
+
+export default components;
